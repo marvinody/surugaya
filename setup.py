ceda37950bdad29dadba6a09412949d01180f1af
Manuel Bugallo <marvinody@gmail.com>
2020-03-02 20:57:08 -0500

    updates setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 import setuptools
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='suruguya',
-    version='0.0.3',
+    version='0.0.4',
     author='marvinody',
     author_email='manny@sadpanda.moe',
     description='suruguya api-like wrapper',
@@ -16,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://bitbucket.org/marvinody/suruguya/',
     packages=setuptools.find_packages(),
-    install_requires=requirements,
+    install_requires=['beautifulsoup4==4.7.1', 'certifi==2019.3.9', 'chardet==3.0.4', 'idna==2.8', 'requests==2.21.0', 'soupsieve==1.9', 'urllib3==1.24.1'],
     classifiers=[
         "Programming Language :: Python :: 3.5",
     ]
