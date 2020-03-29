88648b457aecb41592c215b4559514a3065eb2cd
Manuel Bugallo <marvinody@gmail.com>
2020-03-28 22:32:08 -0400

    chore: version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='suruguya',
-    version='0.0.4',
+    version='0.0.5',
     author='marvinody',
     author_email='manny@sadpanda.moe',
     description='suruguya api-like wrapper',
